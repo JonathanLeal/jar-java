990c2c1dc4c5a8c64793e9c2d9693bf18c8316c9
JonathanLeal <jonathan.e.leal.m@gmail.com>
2024-09-27 14:16:31 -0600

    Subiendo el jar de la libreria

---
+++ src/Aritmetica/MathOperations.java
@@ -0,0 +1,22 @@
+package Aritmetica;
+
+public class MathOperations {
+    public static double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public static double restar(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public static double dividir(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }
+        return a / b;
+    }
+}
